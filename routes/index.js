865129a747bcef4a6efffdb3eae1ab82d736b56c
yeshwanth vuppu <vyeshwanth123@gmail.com>
2017-06-18 18:05:46 +0530

    Added mongoose and database connection

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express'),
     router  = express.Router();
 
-router.get('/', function(req, res){
-    res.render('index');
-});
+// import controllers
+
+var indexController = require('./../controllers/indexController');
+
+router.get('/', indexController.index);
 
 module.exports = router;
