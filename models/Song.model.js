60facb79d43d60d3672027174d86462318d1201c
vyeshwanth <vyeshwanth123@gmail.com>
2017-07-17 17:52:58 +0530

    Change artists type to Array of type string

--- models/Song.model.js
+++ models/Song.model.js
@@ -5,9 +5,9 @@ var songSchema = new Schema({
     name : String,
     album : {
         name : String,
-        artists : String
+        artists : [String]
     },
-    artists : String,
+    artists : [String],
     path : String,
     inQueue : Boolean,
     timeAddedToQueue : {type : Date, default : Date.now}
