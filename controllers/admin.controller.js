8db5a5164d1e60d0b353e301605e6086942d95ce
vyeshwanth <vyeshwanth123@gmail.com>
2017-07-19 23:01:49 +0530

    Add admin page files

---
+++ controllers/admin.controller.js
@@ -0,0 +1,13 @@
+module.exports.getLoginPage = function(req, res) {
+    if (req.isAuthenticated()) {
+        res.redirect('/admin/home');
+    }
+    res.render('admin/admin');
+};
+
+module.exports.getHomePage = function(req, res) { 
+    if (!req.isAuthenticated()) {
+        res.redirect('admin');
+    }
+    res.render('admin/home');
+};
