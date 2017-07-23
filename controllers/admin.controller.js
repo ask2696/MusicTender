1b37578e1675d2a458ca5a2d088adbeb16523c7f
vyeshwanth <vyeshwanth123@gmail.com>
2017-07-23 13:58:44 +0530

    Add files for songs broser page

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -1,7 +1,15 @@
 module.exports.getLoginPage = function(req, res) {
+    if (req.isAuthenticated()) {
+        res.redirect('/admin/home');
+        return;
+    }
     res.render('admin/index');
 };
 
 module.exports.getHomePage = function(req, res) {
-    res.send('Welcome Home');
+    if (!req.isAuthenticated()) {
+        res.redirect('/admin');
+        return;
+    }
+    res.render('admin/home');
 };
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,7 @@ var router = express.Router();
 var indexController = require('./../controllers/index.controller');
 
 router.get('/', indexController.index);
+router.get('/music', function(req, res) {
+    res.render('music');
+})
 module.exports = router;
