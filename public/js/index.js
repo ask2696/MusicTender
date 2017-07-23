4715942a50d1c70f83f7d3a56ce22ee17dd777a7
vyeshwanth <vyeshwanth123@gmail.com>
2017-07-23 13:49:51 +0530

    Use jquery hide and show instead of changing css display property

--- public/js/index.js
+++ public/js/index.js
@@ -1,21 +1,20 @@
-$( function(){
+$( function() {
    var $upvotedSongsButt = $('#upvoted-songs-butt');
    var $upcomingSongsButt = $('#upcoming-song-butt');
-   var $upvatedSongsList = $('#upvoted-song-list');
+   var $upvotedSongsList = $('#upvoted-song-list');
    var $upcomingSongsList = $('#upcoming-song-list');
 
-   $upvotedSongsButt.click(function(){
+   $upvotedSongsButt.click(function() {
        $upcomingSongsButt.removeClass('active');
        $(this).addClass('active');
-       $upcomingSongsList.css('display', 'none');
-       $upvatedSongsList.css('display', 'block');
-   })
+       $upcomingSongsList.hide();
+       $upvotedSongsList.show();
+   });
 
-   $upcomingSongsButt.click(function(){
+   $upcomingSongsButt.click(function() {
        $upvotedSongsButt.removeClass('active');
        $(this).addClass('active');
-       $upcomingSongsList.css('display', 'block');
-       $upvatedSongsList.css('display', 'none');
-   })
-
-})
+       $upcomingSongsList.show();
+       $upvotedSongsList.hide();
+   });
+});
